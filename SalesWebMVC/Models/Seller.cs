07ee0e1ed6ff1430fcaa87f2c877a88f1e3a8529
Jonas Melo <jonasmoralesmelo@gmail.com>
2020-06-27 21:05:21 -0300

    Foreing key not null

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -14,7 +14,8 @@ namespace SalesWebMVC.Models
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
-
+        public int DepartmentId { get; set; } //Com isso, avisa ao EntityFramework para ele garantir que esse Id tem que existir
+        // uma vez que o tipo int não pode ser nulo.
         public Seller() { }
 
         public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
